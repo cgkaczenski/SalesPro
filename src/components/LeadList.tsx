734530757301cbb8d482ce4765113b812932558a
cgkaczenski <kzpulaski@gmail.com>
2024-03-11 05:10:58 -0500

    Add minor style changes to lead list

--- src/components/LeadList.tsx
+++ src/components/LeadList.tsx
@@ -1,8 +1,30 @@
+import Image from "next/image";
+import { UserCircleIcon } from "@heroicons/react/24/solid";
+
 export default function LeadList() {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
       <li>
-        <button>Lead Name</button>
+        <button className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-slate-100 focus:bg-slate-100 transition">
+          <UserCircleIcon
+            className="text-gray-300"
+            aria-hidden="true"
+            width={45}
+            height={45}
+          />
+          <p className="font-semibold flex items-center">Chris</p>
+        </button>
+      </li>
+      <li>
+        <button className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-slate-100 focus:bg-slate-100 transition">
+          <UserCircleIcon
+            className="text-gray-300"
+            aria-hidden="true"
+            width={45}
+            height={45}
+          />
+          <p className="font-semibold flex items-center">Ben</p>
+        </button>
       </li>
     </ul>
   );
